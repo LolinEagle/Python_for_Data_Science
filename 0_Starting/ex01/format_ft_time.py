14250001b8c9c71e8bc0a25fe1bf44813cf52fc4
François <francois-lolin@live.fr>
2023-11-30 17:45:00 +0100

    First commit

---
+++ 0_Starting/ex01/format_ft_time.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+now = datetime.utcnow()			# Get the current date and time
+timestamp = int(now.timestamp())# Calculate the seconds since January 1, 1970 (Unix timestamp)
+
+print(f"Seconds since January 1, 1970: {timestamp:,.4f} or {timestamp:.2e} in scientific notation\n{now.strftime('%b %d %Y')}")
---
+++ 0_Starting/ex02/find_ft_type.py
@@ -0,0 +1,15 @@
+def all_thing_is_obj(object: any) -> int:
+	object_type = type(object)
+	object_dict = {
+		list: "List",
+		tuple: "Tuple",
+		set: "Set",
+		dict: "Dict",
+		str: "Brian is in the kitchen",
+	}
+
+	if object_type == int:
+		print("Type not found")
+	else:
+		print(f"{object_dict[object_type]} : {object_type}")
+	return(42)
