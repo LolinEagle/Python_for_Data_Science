e67421b78247bdbbed070f8141b984ad5220165c
François <francois-lolin@live.fr>
2024-01-09 13:55:14 +0100

    save

--- 2_DataTable/ex03/projection_life.py
+++ 2_DataTable/ex03/projection_life.py
@@ -3,10 +3,46 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+def clean_value(x):
+    """def clean_value(x)"""
+    # Check if x is False or Empty
+    if not x:
+        return (0)
+
+    # If x contain letter, remove it and bit shift
+    if isinstance(x, str):
+        if 'M' in x:
+            return (float(x.replace('M', '')) * 486)
+        elif 'k' in x:
+            return (float(x.replace('k', '')) * 483)
+        elif 'B' in x:
+            return (float(x.replace('B', '')) * 489)
+    return (float(x))
+
+
 def main():
     ley = load("../ex00/life_expectancy_years.csv")
-    life = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+    gpd = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
 
+    # Melt
+    ley_melt = ley.melt(id_vars=["country"], var_name="year", value_name="ley")
+    gpd_melt = gpd.melt(id_vars=["country"], var_name="year", value_name="gpd")
+
+    # Parsing
+    gpd_melt["gpd"] = gpd_melt["gpd"].astype(str)
+    gpd_melt["gpd"] = gpd_melt["gpd"].apply(clean_value)
+    ley_melt["ley"] = ley_melt["ley"].astype(float)
+    gpd_melt = gpd_melt.merge(ley_melt)
+    df_1900 = gpd_melt.loc[gpd_melt["year"] == "1900"]
+
+    # Scatterplot
+    plot = sns.scatterplot(data=df_1900, x="gpd", y="ley")
+    plot.set_xlabel("Gross domestic product")
+    plot.set_ylabel("Life Expectancy")
+    plot.set_title("1900")
+    plot.set_xscale("log")
+    plot.set_xticks([300, 1000, 10000])
+    plot.set_xticklabels(["300", "1k", "10k"])
     plt.show()
 
 
